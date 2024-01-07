5d80fa86c571474eea64929aaa30a16eb87fbdea
Your Name <you@example.com>
2024-01-07 19:18:19 +0530

    first commit

---
+++ 029_-_books/src/components/BookList.js
@@ -0,0 +1,15 @@
+
+import useBooksContext from '../hooks/use-book-context';
+import BookShow from './BookShow';
+
+function BookList() {
+  const { books } = useBooksContext();
+  
+  const renderedBooks = books.map((book) => {
+    return <BookShow  key={book.id} book={book} />;
+  });
+
+  return <div className="book-list">{renderedBooks}</div>;
+}
+
+export default BookList;
